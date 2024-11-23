68a4786216107a64affef32f593b9e3c9a52fda7
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-23 18:23:29 +0900

    Title: 4번의 크기 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241123/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,28 @@
+a = int(input())
+
+arr = input().split()
+b = int(arr[0])
+c = int(arr[1])
+d = int(arr[2])
+e = int(arr[3])
+
+if a > b:
+    print("1")
+else:
+    print("0")
+
+if a > c:
+    print("1")
+else:
+    print("0")
+
+if a > d:
+    print("1")
+else:
+    print("0")
+
+if a > e:
+    print("1")
+else:
+    print("0")
+
