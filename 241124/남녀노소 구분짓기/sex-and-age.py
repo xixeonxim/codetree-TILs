dec10ce5bd2998c1aebab149d276edcac43faaac
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-24 00:04:19 +0900

    Title: 남녀노소 구분짓기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241124/남녀노소 구분짓기/sex-and-age.py
@@ -0,0 +1,14 @@
+gen = int(input())
+age = int(input())
+
+if gen == 0:
+    if age >= 19:
+        print("MAN")
+    else:
+        print("BOY")
+
+if gen == 1:
+    if age >= 19:
+        print("WOMAN")
+    else:
+        print("GIRL")
