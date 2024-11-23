839fb9b3e6955ed9c7713ace9418caab89d92e73
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-23 17:47:35 +0900

    Title: 특정 조건 두 정수 비교, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241123/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,13 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+
+if a < b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
+if a == b:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
