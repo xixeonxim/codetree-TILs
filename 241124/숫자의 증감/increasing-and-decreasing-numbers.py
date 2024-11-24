5feb75f1ea5c7591923ab2ca0d3fb19d2f43a227
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-24 21:38:42 +0900

    Title: 숫자의 증감, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241124/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,10 @@
+arr = input().split()
+c = arr[0]
+n = int(arr[1])
+
+if c == "A":
+    for i in range(1, n+1, 1):
+        print(i, end = " ")
+elif c == "D":
+    for i in range(n, 0, -1):
+        print(i, end = " ")
