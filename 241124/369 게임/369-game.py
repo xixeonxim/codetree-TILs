1bdc45b80264cb770ae450ea80291d5de8e07cd5
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-24 22:50:50 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241124/369 게임/369-game.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print("0", end = " ")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 ==9:
+        print("0", end = " ")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+        print("0", end = " ")
+    else:
+        print(i, end = " ")
