1f4f040a4ddf0bd9fcd840111d595aaecb582031
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-11-24 21:50:29 +0900

    Title: 높은 수에서 낮은 수까지, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241124/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 241124/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -3,5 +3,9 @@ arr = input().split()
 a = int(arr[0])
 b = int(arr[1])
 
-for i in range(b,a-1, -1):
-    print(i, end = " ")
+if b >= a:
+    for i in range(b,a-1, -1):
+        print(i, end = " ")
+else:
+    for i in range(a, b-1, -1):
+        print(i, end = " ")
