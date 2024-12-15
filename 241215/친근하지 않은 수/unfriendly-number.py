e061d6a4d85ea846bc84a0b60ad0a98acdf09bd5
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-12-15 22:25:17 +0900

    Title: 친근하지 않은 수, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241215/친근하지 않은 수/unfriendly-number.py
+++ 241215/친근하지 않은 수/unfriendly-number.py
@@ -2,7 +2,7 @@ n = int(input())
 cnt = 0 
 
 for i in range(1, n+1):
-    if n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
+    if i % 2 == 0 or i % 3 == 0 or i % 5 == 0:
         continue    
     cnt += 1
     
