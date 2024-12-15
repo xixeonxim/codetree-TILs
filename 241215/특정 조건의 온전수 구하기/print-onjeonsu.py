91814eb99ccee985d97a487b794611d1e4720abc
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-12-15 23:38:53 +0900

    Title: 특정 조건의 온전수 구하기, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241215/특정 조건의 온전수 구하기/print-onjeonsu.py
+++ 241215/특정 조건의 온전수 구하기/print-onjeonsu.py
@@ -1,13 +1,12 @@
+# 변수 선언 및 입력
 n = int(input())
 
-for i in range(1, n+1):
-    if i % 2 == 0:
-        continue
-    if i % 10 == 5:
-        continue
-    if i % 3 == 3 and i % 9 != 0:
-        continue
-    print(i, end =" ")
-
-    
-
+# 1부터 n까지의 수 중, 온전수를 찾아 출력합니다.
+for i in range(1, n + 1):
+	if i % 2 == 0:
+		continue
+	if i % 10 == 5:
+		continue
+	if i % 3 == 0 and i % 9 != 0:
+		continue
+	print(i, end=" ")
