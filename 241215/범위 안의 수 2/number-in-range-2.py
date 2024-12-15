dc4885d1e32921f4c965304c7967dbe2be9bdf52
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-12-15 20:03:14 +0900

    Title: 범위 안의 수 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241215/범위 안의 수 2/number-in-range-2.py
@@ -0,0 +1,14 @@
+sum_val = 0
+avg = 0
+cnt = 0
+
+for _ in range(10):
+    a = int(input())
+    if a >= 0 and a <= 200:
+        sum_val += a
+        cnt += 1
+
+avg = sum_val / cnt
+
+print(f"{sum_val} {avg:.1f}")
+
