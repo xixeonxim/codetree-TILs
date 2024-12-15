37579cbf2606a900e858f2e1fc23161314985bdc
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2024-12-15 19:53:35 +0900

    Title: 5의 배수의 합, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241215/5의 배수의 합/sum-of-specific-condition-multiple.py
+++ 241215/5의 배수의 합/sum-of-specific-condition-multiple.py
@@ -3,13 +3,14 @@ a = int(arr[0])
 b = int(arr[1])
 
 sum_val = 0 
+
 if a <= b:
     for i in range(a, b+1):
         if i % 5 == 0:
             sum_val += i
 
 else:
-     for i in range(a, b+1):
+     for i in range(b, a+1):
         if i % 5 == 0:
             sum_val += i
 
