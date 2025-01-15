9bcea2e3387744f896eed68329a0320c411ed6a8
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2025-01-15 18:46:26 +0900

    Title: 모두 3의 배수, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250115/모두 3의 배수/all-multiples-of-3.py
+++ 250115/모두 3의 배수/all-multiples-of-3.py
@@ -1,17 +1,15 @@
-#5개의 정수 받기
-arr = list(map(int, input().split())) #입력을 정수 리스트로 변환
 
-#모든 3의 배수인지 확인하는 변수 
-is_all_multiple_3 = True
+p = True
+for _ in range(5):
+    n = int(input())
 
-#모두 3의 배수인지 확인
-
-for num in arr:
-    if num % 3 != 0:
-        is_all_multiple_3 = False
+    if n % 3 == 0:
+        continue
+    else:
+        p = False
         break
-
-if is_all_multiple_3:
+    
+if p == True:
     print(1)
 else:
     print(0)
