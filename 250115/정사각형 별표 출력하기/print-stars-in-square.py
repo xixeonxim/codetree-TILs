9af8c080fbfeaf796fb253ea17f8a3de42e04018
Jiyeon Kim <101583454+xixeonxim@users.noreply.github.com>
2025-01-15 19:08:50 +0900

    Title: 정사각형 별표 출력하기, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250115/정사각형 별표 출력하기/print-stars-in-square.py
+++ 250115/정사각형 별표 출력하기/print-stars-in-square.py
@@ -4,7 +4,7 @@ n = int(input())
 #한 줄에 * 4개씩 출력하기 -> 안쪽 가로
 # 이후 바깥 쪽에 4줄로 출력하기 위해 반복문 4번
 
-for _ in range (4):
+for _ in range (n):
     for _ in range(4):
         print("*", end = "")
     print()
